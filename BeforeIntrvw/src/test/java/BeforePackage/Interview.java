e2c930f2ffb979bf710106d40b66299c6def26f9
sumanmitra19 <mitr.suman99@gmail.com>
2019-01-10 23:13:10 +0530

    Adding few text

---
+++ BeforeIntrvw/src/test/java/BeforePackage/Interview.java
@@ -0,0 +1,26 @@
+package BeforePackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Interview {
+	
+	@Test
+	public void login()
+	{
+		System.out.println("Test Case started");
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\Softy\\Cell\\Drivers\\Chrome\\chromedriver.exe");
+		WebDriver dr = new ChromeDriver();
+		dr.get("https://pi.cashpoint.com");
+		dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		dr.findElement(By.id("username")).sendKeys("pitest1");
+		dr.findElement(By.id("password")).sendKeys("$Pitest#1");
+		dr.findElement(By.id("submit")).click();
+		System.out.println("Test Case has been finished");
+	}
+	
+}
